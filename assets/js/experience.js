bfec9855e86de19899b205f819c058c651ae2dd7
Sudip Kandel <37169547+sudipkandel123@users.noreply.github.com>
2025-04-10 11:06:08 +0100

    Made changes with the experience and education section

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,25 +5,25 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "AI/ML Engineer Senior Analyst",
+    cardImage: "assets/images/experience-page/accenture.png",
+    place: "Accenture",
+    time: "(Mar, 2020 - Sept, 2024)",
+    desp: "<li>Specialized in developing and maintaing LLMs, Finetuning models, Natural language processing and deployment. Worked in developing end to end Machine learning solutions specially in Azure.</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    title: "Reporting Analyst",
+    cardImage: "assets/images/experience-page/exl.jpg",
+    place: "EXL Services",
+    time: "(Aug 2019 - Mar, 2020)",
+    desp: "<li>Working with Machine learning solutions for CI/CD using FastAPI. developed robust strategy and consulting skills to deliver impactful solutions to clients. </li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Intern",
+    cardImage: "assets/images/experience-page/fixolla.jpg",
+    place: "Fixolla",
+    time: "(Mar, 2020 - Jul, 2020)",
+    desp: "<li>Worked as an Intern in helping boost Fixolla's Ad Revenue with GoogleAds360 </li>",
   },
 ];
 
